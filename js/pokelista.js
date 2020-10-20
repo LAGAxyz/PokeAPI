1e889ed97e76dbbd2e9e536196c1bac4c2270c3b
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2020-10-20 14:50:06 -0500

    Lista de pokemones OK

--- js/pokelista.js
+++ js/pokelista.js
@@ -6,35 +6,30 @@ const pokeNext = document.getElementById("pokeNext");
 const pokeLista = document.getElementById("pokeLista");
 
 const URL = "https://pokeapi.co/api/v2/pokemon";
-let limit = 10;
+let limit = 20;
+let offset = 0;
+let dataP = null;
+let dataN = null;
 
 pokeCantidad.onchange = ()=> {
-    let option = pokeCantidad.options[pokeCantidad.selectedIndex].value;
-    setPaginacion(option);
+    limit = pokeCantidad.options[pokeCantidad.selectedIndex].value;
+    setPaginacion(limit, offset);
 };
 
-const setPaginacion = (limit)=> {
-    fetch(`${URL}?limit=${limit}&offset=0`).then((consulta)=>{
-        consulta.json().then((data)=>{
-            dibujarPokemones(data);
-        })
-    })
+const setPaginacion = (limit, offset)=> {
+    fetch(`${URL}?limit=${limit}&offset=${offset}`).then((consulta)=>{
+        consulta.json().then((datos)=>{
+            dibujarPokemones(datos);
+        });
+    });
 };
 setPaginacion();
 
 const dibujarPokemones = (data)=> {
+    let datos = [...data.results];
+    dataP = data.previous;
+    dataN = data.next;
     pokeLista.innerHTML = "";
-
-    pokePrevious.onclick = ()=>{
-        if(data.previous === null){return;}
-    };
-
-    pokeNext.onclick = ()=> {
-        if(data.next === null){return;}
-    };
-
-    datos = [...data.results];
-
     let divFake = new DocumentFragment();
 
     for (let item=0; item<datos.length; item++){
@@ -51,9 +46,14 @@ const dibujarPokemones = (data)=> {
         cuerpo.classList.add("card-body");
         texto.classList.add("card-text", "text-center", "font-weight-bold", "text-primary", "h3");
         
-        img.src="https://via.placeholder.com/300x300";
         texto.innerText = datos[item].name;
-    
+
+        fetch(`${URL}/${item+1}`).then((consulta)=>{
+            consulta.json().then((data)=>{
+                img.src = data.sprites.other["official-artwork"].front_default;
+            });
+        });
+
         figure.appendChild(img);
         cuerpo.appendChild(texto);
         card.appendChild(figure);
@@ -65,87 +65,20 @@ const dibujarPokemones = (data)=> {
     pokeLista.appendChild(divFake);
 };
 
+pokePrevious.onclick = ()=>{
+    if(dataP === null){return;}
+    fetch(dataP).then((consulta)=>{
+        consulta.json().then((datos)=>{
+            dibujarPokemones(datos);
+        });
+    });
+};
 
-/*
-  
-//   const dibujarListadoPokemon = (arreglo) => {
-//     pokePrevious.onclick = () => {
-//       getListadoPokemon(arreglo.previous).then((resultado) => {
-//         dibujarListadoPokemon(resultado);
-//       });
-//     };
-  
-//     pokeNext.onclick = () => {
-//       getListadoPokemon(arreglo.next).then((resultado) => {
-//         dibujarListadoPokemon(resultado);
-//       });
-//     };
-  
-//     let fetchs = [];
-//     let jsons = [];
-  
-//     let miPromesa = new Promise((resolve, reject) => {
-//       arreglo.results.forEach((element) => {
-//         let peticion = fetch(element.url);
-//         fetchs.push(peticion);
-//       });
-  
-//       Promise.all(fetchs).then((peticiones) => {
-//         peticiones.forEach((pet) => {
-//           jsons.push(pet.json());
-//         });
-//         Promise.all(jsons).then((rptas) => {
-//           resolve(rptas);
-//         });
-//       });
-//     });
-  
-//     miPromesa.then((rpta) => {
-//       // EN ESTE PUNTO YA TENEMOS LA DATA QUE DEBEMOS DE MOSTRAR EN LA PAGINA
-  
-//       rpta.forEach((data) => {
-  
-//         if (data.sprites.other.dream_world.front_default === null) {
-//           if (data.sprites.other["official-artwork"].front_default === null) {
-//             if (data.sprites.front_default === null) {
-//               listadoCardImagen.src = "../img/Pokeball girando.gif";
-//             } else {
-//               listadoCardImagen.src = data.sprites.front_default;
-//             }
-//           } else {
-//             listadoCardImagen.src =
-//               data.sprites.other["official-artwork"].front_default;
-//           }
-//         } else {
-//           listadoCardImagen.src = data.sprites.other.dream_world.front_default;
-//         }
-  
-//       });
-//       cargando.setAttribute("hidden", true);
-//     });
-//   };
-  
-//   // GET LISTADO POKEMON
-//   const getListadoPokemon = (enlace) => {
-//     cargando.removeAttribute("hidden");
-  
-//     return new Promise((resolve, reject) => {
-//       fetch(enlace).then((peticion) => {
-//         peticion.json().then((data) => {
-//           resolve(data);
-//         });
-//       });
-//     });
-//   };
-  
-//   getListadoPokemon(`${URL_BACKEND}${URL_RESOURCE}?offset=0&limit=${limit}`).then(
-//     (resultado) => {
-//       console.log(resultado.results);
-//       console.log(resultado);
-//       dibujarListadoPokemon(resultado);
-      
-//     }
-//   );
-
-
-*/
+pokeNext.onclick = ()=> {
+    if(dataN === null){return;}
+    fetch(`${dataN}`).then((consulta)=>{
+        consulta.json().then((datos)=>{
+            dibujarPokemones(datos);
+        });
+    });
+};
