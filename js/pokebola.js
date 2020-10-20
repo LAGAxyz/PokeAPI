582110daf81166adb880e0a225b8b92f162b3248
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2020-10-20 01:08:10 -0500

    Página pokebola.html OK

---
+++ js/pokebola.js
@@ -0,0 +1,23 @@
+
+const pokebola = document.getElementById("pokebola");
+const cboAnimaciones = document.getElementById("cboAnimaciones");
+
+const animaciones = [
+    "bounce", "flash", "pulse", "rubberBand", "shakeX", "shakeY",
+    "swing", "tada", "wobble", "jello", "heartBeat",
+]
+
+const listarAnimaciones = ()=> {
+    for(let item=0; item<animaciones.length; item++){
+        const a = document.createElement("a");
+        a.classList.add("dropdown-item", "cboAnimacion");
+        a.innerText = animaciones[item];
+
+        a.onclick = ()=> {
+            pokebola.style.animation = `${animaciones[item]} 3s infinite`;
+        };
+
+        cboAnimaciones.appendChild(a);
+    }
+};
+listarAnimaciones();
