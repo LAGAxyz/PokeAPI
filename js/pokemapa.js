128a7a67053b75a3a1a506a3047f84658ff89bad
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2020-10-23 12:38:02 -0500

    Página pokemapa.html responsive agregada (en pruebas)

---
+++ js/pokemapa.js
@@ -0,0 +1,37 @@
+
+const elemento_mapa = document.getElementById("google_canvas");
+google.maps.event.addDomListener(window, 'load', dibujarMapa);
+var marcador_tiempo_real, mapa;
+
+function dibujarMapa(){
+    var opcionesMapa = {zoom: 15, mapTypeId: google.maps.MapTypeId.ROADMAP}
+    mapa = new google.maps.Map(elemento_mapa, opcionesMapa);
+
+    navigator.geolocation.getCurrentPosition(function(posicion){
+        var geolocalizacion = new google.maps.LatLng(posicion.coords.latitude, posicion.coords.longitude);
+        var marcador = new google.maps.Marker({
+            map: mapa,
+            position: geolocalizacion,
+            visible: true,
+            icon: "./img/pokepunto.png"
+        });
+        marcador_tiempo_real = new google.maps.Marker({
+            map: mapa,
+            position: geolocalizacion,
+            visible: true,
+            icon: "./img/pokepunto.png"
+        });
+        mapa.setCenter(geolocalizacion);
+        navigator.geolocation.watchPosition(actualizarPosicion, function(e){
+            alert("Error: " + e);
+        }, {maximumAge: 0});
+    }, function(e){
+        alert("Error: " + e);
+    });
+}
+
+function actualizarPosicion(posicion){
+    var geolocalizacion = new google.maps.LatLng(posicion.coords.latitude, posicion.coords.longitude);
+    marcador_tiempo_real.setPosition(geolocalizacion);
+    mapa.setCenter(geolocalizacion);
+}
