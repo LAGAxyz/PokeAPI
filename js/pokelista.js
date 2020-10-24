9b1929357c6bd64cc364d7376ad180abf06dd335
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2020-10-24 17:38:04 -0500

    SR agregado y página pokelista.html responsiva con pokemones listados

--- js/pokelista.js
+++ js/pokelista.js
@@ -1,36 +1,17 @@
 
-const pokeCarga = document.getElementById("pokeCarga");
-const pokePrevious = document.getElementById("pokePrevious");
-const pokeCantidad = document.getElementById("pokeCantidad");
-const pokeNext = document.getElementById("pokeNext");
-const pokeLista = document.getElementById("pokeLista");
-
-const URL = "https://pokeapi.co/api/v2/pokemon";
-let limit = 20;
-let offset = 0;
-let dataP = null;
-let dataN = null;
-
-pokeCantidad.onchange = ()=> {
-    limit = pokeCantidad.options[pokeCantidad.selectedIndex].value;
-    setPaginacion(limit, offset);
-};
-
-const setPaginacion = (limit, offset)=> {
-    fetch(`${URL}?limit=${limit}&offset=${offset}`).then((consulta)=>{
+const getPokemones = ()=> {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=893&offset=0").then((consulta)=>{
         consulta.json().then((datos)=>{
-            dibujarPokemones(datos);
+            listarPokemones(datos);
         });
     });
 };
-setPaginacion();
+getPokemones();
 
-const dibujarPokemones = (data)=> {
+const listarPokemones = (data)=> {
     let datos = [...data.results];
-    dataP = data.previous;
-    dataN = data.next;
-    pokeLista.innerHTML = "";
     let divFake = new DocumentFragment();
+    document.getElementById("pokeLista").innerHTML = "";
 
     for (let item=0; item<datos.length; item++){
         let columna = document.createElement("div");
@@ -40,17 +21,19 @@ const dibujarPokemones = (data)=> {
         let cuerpo = document.createElement("div");
         let texto = document.createElement("p");
     
-        columna.classList.add("col-lg-3", "col-md-4", "col-sm-6", "col-10", "my-3", "mx-1");
+        columna.classList.add("col-xl-2", "col-lg-3", "col-md-4", "col-sm-5", "col-10", "my-3", "mx-1", "pokecard");
         card.classList.add("card", "shadow-sm", "bg-white", "rounded", "border-0");
         img.classList.add("card-img-top");
         cuerpo.classList.add("card-body");
-        texto.classList.add("card-text", "text-center", "font-weight-bold", "text-primary", "h3");
+        texto.classList.add("card-text", "text-center", "font-weight-bold", "text-primary", "h4");
         
         texto.innerText = datos[item].name;
 
-        fetch(`${URL}/${item+1}`).then((consulta)=>{
+        fetch(`https://pokeapi.co/api/v2/pokemon/${item+1}`).then((consulta)=>{
             consulta.json().then((data)=>{
-                img.src = data.sprites.other["official-artwork"].front_default;
+                if(data.sprites.other["official-artwork"].front_default){
+                    img.src = data.sprites.other["official-artwork"].front_default;
+                } else {img.src = "./img/pokegiro.gif";}
             });
         });
 
@@ -62,23 +45,5 @@ const dibujarPokemones = (data)=> {
     
         divFake.appendChild(columna);
     }
-    pokeLista.appendChild(divFake);
-};
-
-pokePrevious.onclick = ()=>{
-    if(dataP === null){return;}
-    fetch(dataP).then((consulta)=>{
-        consulta.json().then((datos)=>{
-            dibujarPokemones(datos);
-        });
-    });
-};
-
-pokeNext.onclick = ()=> {
-    if(dataN === null){return;}
-    fetch(`${dataN}`).then((consulta)=>{
-        consulta.json().then((datos)=>{
-            dibujarPokemones(datos);
-        });
-    });
+    document.getElementById("pokeLista").appendChild(divFake);
 };
