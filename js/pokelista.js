3918b05d5335faeecc910b36a13e294ad4b80663
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2020-10-25 15:32:55 -0500

    Implementación de Intersection Observer a pokelista.html

--- js/pokelista.js
+++ js/pokelista.js
@@ -1,8 +1,8 @@
 
 const getPokemones = ()=> {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=893&offset=0").then((consulta)=>{
-        consulta.json().then((datos)=>{
-            listarPokemones(datos);
+        consulta.json().then((data)=>{
+            listarPokemones(data);
         });
     });
 };
@@ -14,36 +14,63 @@ const listarPokemones = (data)=> {
     document.getElementById("pokeLista").innerHTML = "";
 
     datos.forEach((dato)=>{
-        let columna = document.createElement("div");
+        let row = document.createElement("div");
         let card = document.createElement("div");
         let figure = document.createElement("figure");
         let img = document.createElement("img");
         let cuerpo = document.createElement("div");
         let texto = document.createElement("p");
-    
-        columna.classList.add("col-xl-2", "col-lg-3", "col-md-4", "col-sm-5", "col-10", "my-3", "mx-1", "pokecard");
+
+        row.setAttribute("url", `${dato.url}`);
+
+        row.id = `${dato.url}`;
+        img.id = `PF${dato.url}`;
+        texto.id = `PN${dato.url}`;
+
+        row.classList.add("col-xl-2", "col-lg-3", "col-md-4", "col-sm-5", "col-10", "my-3", "mx-1", "pokecard");
         card.classList.add("card", "shadow-sm", "bg-white", "rounded", "border-0");
         img.classList.add("card-img-top");
         cuerpo.classList.add("card-body");
         texto.classList.add("card-text", "text-center", "font-weight-bold", "text-primary", "h4");
         
-        texto.innerText = dato.name;
-    
-        fetch(`${dato.url}`).then((consulta)=>{
-            consulta.json().then((data)=>{
-                data.sprites.other["official-artwork"].front_default ?
-                img.src = data.sprites.other["official-artwork"].front_default :
-                img.src = "./img/pokegiro.gif";
-            });
-        });
+        texto.innerText = "Cargando...";
+        img.src = "http://placehold.it/290x290?text=Cargando...";
     
         figure.appendChild(img);
         cuerpo.appendChild(texto);
         card.appendChild(figure);
         card.appendChild(cuerpo);
-        columna.appendChild(card);
+        row.appendChild(card);
     
-        divFake.appendChild(columna);
+        io.observe(row);
+        divFake.appendChild(row);
     });
+
     document.getElementById("pokeLista").appendChild(divFake);
+};
+
+let io = new IntersectionObserver((entries)=>{
+    entries.forEach(({target, intersectionRatio})=>{
+        if(intersectionRatio > 0){
+            let url = target.getAttribute("url");
+            traerPokemon(target, url);
+        }
+    })
+}, {});
+
+const traerPokemon = async (elemento, url)=> {
+    const peticion = await fetch(url);
+    const data = await peticion.json();
+
+    const row = document.getElementById(`${url}`);
+    const pokeFoto = document.getElementById(`PF${url}`);
+    const pokeNombre = document.getElementById(`PN${url}`);
+
+    pokeNombre.innerText = data.name;
+
+    data.sprites.other["official-artwork"].front_default ?
+    pokeFoto.src = data.sprites.other["official-artwork"].front_default :
+    pokeFoto.src = "./img/pokegiro.gif";
+
+    io.unobserve(row);
 };
